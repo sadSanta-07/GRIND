897c9759c3404ae54166264ad80b3c95f03324ff
pechkas <iffcogc34@gmail.com>
2026-06-05 16:32:03 +0530

    Add implementation for Dominopiling problem

---
+++ codeforces/A.Dominopiling.cpp
@@ -0,0 +1,26 @@
+// 50A
+
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int Dominopiling(int n, int m)
+    {
+        return (n * m) / 2;
+    }
+};
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    cout << (n * m) / 2;
+
+    return 0;
+}
