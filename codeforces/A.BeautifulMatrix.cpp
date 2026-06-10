d47ae79a31341423aaf8cae792b7e12de616b861
pechkas <iffcogc34@gmail.com>
2026-06-10 11:30:15 +0530

    Add implementation for Beautiful Matrix problem (263A)

---
+++ codeforces/A.BeautifulMatrix.cpp
@@ -0,0 +1,21 @@
+// 263A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cin >> x;
+            if (x == 1)
+            {
+                cout << abs(i - 2) + abs(j - 2);
+                return 0;
+            }
+        }
+    }
+}
