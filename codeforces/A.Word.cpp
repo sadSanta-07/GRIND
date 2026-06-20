014fb6fb3561c57328b4ed2c38a88bedc458959b
pechkas <iffcogc34@gmail.com>
2026-06-20 10:05:35 +0530

    Add implementation for the "A. Word" problem

---
+++ codeforces/A.Word.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int lower = 0, upper = 0;
+
+    for(char c : s) {
+        if(islower(c))
+            lower++;
+        else
+            upper++;
+    }
+
+    if(lower >= upper) {
+        for(char &c : s)
+            c = tolower(c);
+    }
+    else {
+        for(char &c : s)
+            c = toupper(c);
+    }
+
+    cout << s;
+}
