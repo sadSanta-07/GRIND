9434c56fbb72af7978a140e8d11d973d1ee143af
pechkas <iffcogc34@gmail.com>
2026-06-20 10:05:23 +0530

    Add implementation for the "HQ9+" problem

---
+++ codeforces/A.HQ9+.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    for (char c : s)
+    {
+        if (c == 'H' || c == 'Q' || c == '9' || c == '+')
+        {
+            cout << "YES";
+            return 0;
+        }
+    }
+
+    cout << "NO";
+    return 0;
+}
