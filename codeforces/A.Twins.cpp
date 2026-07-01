b3c5d9b1478a5fa78789e840033414e4e9f5b5b5
pechkas <iffcogc34@gmail.com>
2026-07-01 15:30:47 +0530

    Refactor A.Twins.cpp for improved readability and structure

--- codeforces/A.Twins.cpp
+++ codeforces/A.Twins.cpp
@@ -1,2 +1,34 @@
 //160A
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+    int total = 0;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        total += a[i];
+    }
+
+    sort(a.begin(), a.end(), greater<int>());
+
+    int mySum = 0;
+    int count = 0;
+
+    for (int coin : a) {
+        mySum += coin;
+        count++;
+
+        if (mySum > total - mySum) {
+            cout << count;
+            break;
+        }
+    }
+
+    return 0;
+}
