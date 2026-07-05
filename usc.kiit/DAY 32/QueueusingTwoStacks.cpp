9b7bf97108f069fe4846b5f4971af90654864d12
pechkas <iffcogc34@gmail.com>
2026-07-06 00:50:52 +0530

    Add implementation of queue using two stacks

---
+++ usc.kiit/DAY 32/QueueusingTwoStacks.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class queue
+{
+    stack<int> s1, s2;
+
+public:
+    void enqueue(int x)
+    {
+        while (!s1.empty())
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }
+
+        s1.push(x);
+        while (!s2.empty())
+        {
+            s1.push(s2.top());
+            s2.pop();
+        }
+    }
+
+    void dequeue()
+    {
+        if (s1.empty())
+        {
+            return;
+        }
+
+        s1.pop();
+    }
+
+    int front()
+    {
+        if (s1.empty())
+        {
+            return -1;
+        }
+        return s1.top();
+    }
+
+    int size()
+    {
+        return s1.size();
+    }
+    bool empty()
+    {
+        return s1.empty();
+    }
+};
+
+int main()
+{
+    queue q;
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        string s;
+        int x;
+        cin >> s;
+
+        if (s == "REGISTER")
+        {
+            cin >> x;
+            q.enqueue(x);
+        }
+        else if (s == "NEXT")
+        {
+            cout << q.front() << endl;
+            q.dequeue();
+        }
+        else if (q.empty())
+        {
+            cout << "-1" << endl;
+        }
+        else
+        {
+            cout << "INVALID SHII";
+        }
+    }
+}
