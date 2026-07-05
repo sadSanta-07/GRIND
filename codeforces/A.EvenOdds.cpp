614c95957c11a3ecc525ca0f7b2528ac46d34278
pechkas <iffcogc34@gmail.com>
2026-07-06 00:50:34 +0530

    Add solution for Codeforces problem 318A - Even Odds

---
+++ codeforces/A.EvenOdds.cpp
@@ -0,0 +1,19 @@
+// 318A
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long n, k;
+    cin >> n >> k;
+
+    long long oddCount = (n + 1) / 2;
+
+    if (k <= oddCount)
+        cout << 2 * k - 1;
+    else
+        cout << 2 * (k - oddCount);
+
+    return 0;
+}
