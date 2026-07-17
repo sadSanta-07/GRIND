137f3aa5aaae76ce64a92c9f1c36da7bd90fac61
pechkas <iffcogc34@gmail.com>
2026-07-17 22:38:58 +0530

    Add solution for the Maximum in Table problem (509A)

---
+++ codeforces/A.MaximuminTable.cpp
@@ -0,0 +1,22 @@
+// 509A
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int>> a(n, vector<int>(n, 1));
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            a[i][j] = a[i - 1][j] + a[i][j - 1];
+        }
+    }
+    cout << a[n - 1][n - 1];
+
+    return 0;
+}
