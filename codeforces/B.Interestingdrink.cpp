1e2d0643119c2217e296419b03c94b2d4052d791
pechkas <iffcogc34@gmail.com>
2026-07-20 17:55:57 +0530

    Add implementation for B. Interesting Drink problem

---
+++ codeforces/B.Interestingdrink.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a.begin(), a.end());
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        int m;
+        cin >> m;
+
+        cout << upper_bound(a.begin(), a.end(), m) - a.begin() << endl;
+    }
+
+    return 0;
+}
