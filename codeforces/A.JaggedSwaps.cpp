8d60ecf21286693a4966959efa7b3b4cc4d01e47
pechkas <iffcogc34@gmail.com>
2026-07-24 12:38:29 +0530

    Add solution for problem 1896A - Jagged Swaps

---
+++ codeforces/A.JaggedSwaps.cpp
@@ -0,0 +1,34 @@
+// 1896A
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        if (arr[0] == 1)
+        {
+            cout << "YES\n";
+        }
+        else
+        {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
