3fd933d9f7d1ec40af1f5d348dec2071b95043c8
Sahil Singh <iffcogc34@gmail.com>
2026-07-25 17:46:13 +0530

    Time: 0 ms (100%), Space: 71.9 MB (14.8%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,42 +1,19 @@
 class Solution {
 public:
-    int crossSum(vector<int>& nums, int l, int mid, int r) {
-
-        int leftSum = INT_MIN;
-        int sum = 0;
-
-        for (int i = mid; i >= l; i--) {
-            sum += nums[i];
-            leftSum = max(leftSum, sum);
-        }
-
-        int rightSum = INT_MIN;
-        sum = 0;
-
-        for (int i = mid + 1; i <= r; i++) {
-            sum += nums[i];
-            rightSum = max(rightSum, sum);
-        }
-
-        return leftSum + rightSum;
-    }
-
-    int solve(vector<int>& nums, int l, int r) {
-
-        if (l == r)
-            return nums[l];
-
-        int mid = (l + r) / 2;
-
-        int left = solve(nums, l, mid);
-        int right = solve(nums, mid + 1, r);
-        int cross = crossSum(nums, l, mid, r);
-
-        return max({left, right, cross});
-    }
-
     int maxSubArray(vector<int>& nums) {
+        long long sum = 0, mx = LONG_MIN;
 
-        return solve(nums, 0, nums.size() - 1);
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+
+            if (sum > mx) {
+                mx = sum;
+            }
+
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return mx;
     }
 };
