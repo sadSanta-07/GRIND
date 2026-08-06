4c487d4428facb706fbd7e1b14b04ad5108202fe
Sahil Singh <iffcogc34@gmail.com>
2026-08-07 00:36:39 +0530

    Time: 24 ms (44.72%), Space: 102.8 MB (78.32%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -4,22 +4,17 @@ public:
         int n = temperatures.size();
         stack<int> st;
         vector<int> result(n);
-
         for (int i = n - 1; i >= 0; i--) {
-
             while (!st.empty() && temperatures[i] >= temperatures[st.top()]) {
                 st.pop();
             }
-
             if (st.empty()) {
                 result[i] = 0;
             } else {
                 result[i] = st.top() - i;
             }
-
             st.push(i);
         }
-
         return result;
     }
 };
