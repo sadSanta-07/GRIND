8e1fb8ff3b356bb1efadea581cbc2165f3023bc6
Sahil Singh <iffcogc34@gmail.com>
2026-08-22 19:13:09 +0530

    Time: 17 ms (90.66%), Space: 60.4 MB (73.86%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int scheduleCourse(vector<vector<int>>& courses) {
+        sort(courses.begin(), courses.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        priority_queue<int> pq;
+
+        int time = 0;
+
+        for (auto& course : courses) {
+
+            int duration = course[0];
+            int deadline = course[1];
+
+            time += duration;
+            pq.push(duration);
+
+            if (time > deadline) {
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+
+        return pq.size();
+    }
+};
